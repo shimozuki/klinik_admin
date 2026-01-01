a7b21eceb325b067e2954dcabbcad3298456e5fb
robbishimozuki <robbishimozuki@gmail.com>
2026-01-01 16:59:39 +0800

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ use App\Http\Controllers\Api\UserController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 /*
 |--------------------------------------------------------------------------
